ffe1f20521d46f2fd71f79b04313811bfb8d0f81
szopamateusz <github@mateusz-szopa.pl>
2017-02-26 15:07:02 +0100

    Add files via upload

---
+++ skanowanie/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace skanowanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMessage = new System.Windows.Forms.ListBox();
+            this.btScann = new System.Windows.Forms.Button();
+            this.tbhostAdress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.FormattingEnabled = true;
+            this.lbMessage.Location = new System.Drawing.Point(12, 67);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(327, 433);
+            this.lbMessage.TabIndex = 0;
+            // 
+            // btScann
+            // 
+            this.btScann.Location = new System.Drawing.Point(285, 26);
+            this.btScann.Name = "btScann";
+            this.btScann.Size = new System.Drawing.Size(75, 23);
+            this.btScann.TabIndex = 1;
+            this.btScann.Text = "Scan";
+            this.btScann.UseVisualStyleBackColor = true;
+            this.btScann.Click += new System.EventHandler(this.btScann_Click);
+            // 
+            // tbhostAdress
+            // 
+            this.tbhostAdress.Location = new System.Drawing.Point(77, 26);
+            this.tbhostAdress.Name = "tbhostAdress";
+            this.tbhostAdress.Size = new System.Drawing.Size(155, 20);
+            this.tbhostAdress.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Host";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 519);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbhostAdress);
+            this.Controls.Add(this.btScann);
+            this.Controls.Add(this.lbMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbMessage;
+        private System.Windows.Forms.Button btScann;
+        private System.Windows.Forms.TextBox tbhostAdress;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ skanowanie/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace skanowanie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btScann_Click(object sender, EventArgs e)
+        {
+            short[] portList = {20, 21, 22, 23, 53, 70, 80, 109, 110, 119, 143, 161, 162, 443, 3389};
+            string host = tbhostAdress.Text;
+            lbMessage.Items.Add("skanowanie portów dla " + host);
+            lbMessage.Items.Add("to moze chwile potrwac");
+            foreach (var port in portList)
+            {
+                this.Refresh();
+                try
+                {
+                    TcpClient client = new TcpClient(host, port);
+                    lbMessage.Items.Add("port " + port.ToString() + " jest otwarty");
+                }
+                catch
+                {
+                    lbMessage.Items.Add("port " + port.ToString() + "jest zamknięty");
+                }
+            }
+        }
+    }
+}
